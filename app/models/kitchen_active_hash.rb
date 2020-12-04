5c1b97ccf4e5bf20d07a44bcce7e53ad653350a1
Reo-Miyagawa <atethxhrimatggg@gmail.com>
2020-12-04 12:08:45 +0900

    Active_Hashモデルへジャンルの内容を作成

--- app/models/kitchen_active_hash.rb
+++ app/models/kitchen_active_hash.rb
@@ -1,2 +1,8 @@
 class KitchenActiveHash < ActiveHash::Base
+  self.data = [
+    { id: 0, name: '---' },
+    { id: 1, name: '野菜のおかず' }, { id: 2, name: 'お肉のおかず' }, { id: 3, name: '魚介のおかず' },
+    { id: 4, name: 'ごはんもの' }, { id: 5, name: 'パスタ・グラタン' }, { id: 6, name: '麺' },
+    { id: 7, name: 'サラダ' }, { id: 8, name: 'スープ・汁物' }, { id: 9, name: 'パン' }
+  ]
 end
