2cc2821261e723de055f042e26e91a793d09d906
Reo-Miyagawa <atethxhrimatggg@gmail.com>
2020-12-07 12:59:40 +0900

    ログアウト状態のユーザーは、URLを直接入力して食事のリクエスト情報編集ページへ遷移しようとすると、ログインページに遷移

--- app/controllers/kitchens_controller.rb
+++ app/controllers/kitchens_controller.rb
@@ -1,5 +1,5 @@
 class KitchensController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
 
   def index
     @kitchens = Kitchen.all.order('created_at DESC')
